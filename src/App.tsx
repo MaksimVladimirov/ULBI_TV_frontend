24e897db0065f4be25725b7192d1e13a90ccd3e9
Maksim <vl4dimirov@gmail.com>
2023-11-24 14:39:15 +0300

    add: add routes, suspense, react lazy

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,24 @@
+import { MainPageAsync } from "./Pages/MainPage/MainPage.async";
+import { AboutPageAsync } from "./Pages/AboutPage/AboutPage.async";
+import { Route, Routes } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { Suspense } from "react";
+
 import Counter from "./components/Counter";
-import './index.scss'
+import "./index.scss";
 
 const App = () => {
   return (
     <div className="app">
-        <h1>sdffsfsdfsdsfsfdsfd</h1>
+      <Link to={"./"}> Главная</Link>
+      <Link to={"/about"}> О сайте</Link>
+
+      <Suspense fallback={<div> LOADING^^^^</div>}>
+        <Routes>
+          <Route path={"/about"} element={<MainPageAsync />} />
+          <Route path={"/home"} element={<AboutPageAsync />} />
+        </Routes>
+      </Suspense>
       <Counter />
     </div>
   );
