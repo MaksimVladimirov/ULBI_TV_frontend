45040096523c420526920dfa60bf46dbdcd79364
Maksim <vl4dimirov@gmail.com>
2023-11-28 10:42:10 +0300

    fix: fix stylelint

--- src/widgets/NavBar/ui/Navbar.tsx
+++ src/widgets/NavBar/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import { AppLink, AppLinkTheme } from 'shared/ui/AppLink/AppLink';
 import cls from './Navbar.module.scss';
 
 interface NavbarProps {
-  className?: string;
+    className?: string;
 }
 
 export const Navbar = ({ className }: NavbarProps) => (
