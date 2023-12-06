df521c88ba41c7ab2ec053a1766b4b705803b2cd
Maksim <vl4dimirov@gmail.com>
2023-12-06 14:34:20 +0300

    add: add testing library

--- .eslintrc.js
+++ .eslintrc.js
@@ -35,11 +35,7 @@ module.exports = {
         'import/extensions': 'off',
         'import/no-extraneous-dependencies': 'off',
         'no-underscore-dangle': 'off',
-        'i18next/no-literal-string': ['error',
-            {
-                markupOnly: true, ignoreAttribute: ['data-testid', 'to'],
-            },
-        ],
+        'i18next/no-literal-string': ['error', { markupOnly: true }],
         'max-len': ['error', {
             code: 120,
             ignoreComments: true,
