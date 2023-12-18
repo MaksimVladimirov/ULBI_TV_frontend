b6d0071270f5121182e27a3502e9d7710100617e
Maksim <vl4dimirov@gmail.com>
2023-12-18 10:46:42 +0300

    fix: fix sb

--- config/webpack.config.ts
+++ config/webpack.config.ts
@@ -1,7 +1,7 @@
 import webpack from 'webpack';
 import path from 'path';
-import { BuildPaths } from '../build/types/config';
-import { buildCssLoader } from '../build/loaders/buildCssLoader';
+import { BuildPaths } from './build/types/config';
+import { buildCssLoader } from './build/loaders/buildCssLoader';
 
 export default ({ config }: {config: webpack.Configuration}) => {
     const paths: BuildPaths = {
