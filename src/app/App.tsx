f1d29c24176bc147e1312c72c3eb39a6c830892b
Maksim <vl4dimirov@gmail.com>
2023-12-18 15:39:41 +0300

    fix actions

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,10 @@
-import { Suspense } from 'react';
+import React, { Suspense, useEffect } from 'react';
+import './styles/index.scss';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
 import { AppRouter } from 'app/providers/router';
 import { Navbar } from 'widgets/Navbar';
 import { Sidebar } from 'widgets/Sidebar';
-import './styles/index.scss';
 
 function App() {
     const { theme } = useTheme();
